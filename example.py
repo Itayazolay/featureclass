9f29c0c14a17aeba6202037569bae67f8ef364f7
Itayazolay <itayazolay@gmail.com>
2022-01-11 12:45:05 +0200

    Revert "docs, formatting and tests"

--- example.py
+++ example.py
@@ -1,33 +1,29 @@
-from random import randint
+from featureclass import feature, featureclass, feature_names, feature_annotations
+from typing_extensions import get_type_hints
 from typing import List
-
-from featureclass import feature, feature_annotations, feature_names, featureclass
+from random import randint
 
 
 @featureclass
 class MyFeatures:
-    a: int
-
     @feature()
     def feature_1(self) -> int:
         """generate random number"""
-        return randint(0, 10)
+        return randint(0,10)
 
     @feature()
     def feature_2(self) -> int:
         """generate random number"""
-        return randint(0, 10) + self.feature_1
-
+        return randint(0,10) + self.feature_1
+    
     @feature()
     def feature_list(self) -> List[int]:
         """generate random of size 5"""
-        return [randint(0, 10) + self.feature_1 + self.feature_2 for i in range(5)]
+        return [randint(0,10)+self.feature_1+self.feature_2 for i in range(5)]
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     myf = MyFeatures()
+    from statistics import stdev
     print("feature_1", myf.feature_1)
     print("cached", myf.feature_1 == myf.feature_1)
-    print("feature_names:", feature_names(myf))
-    print("feature_annoations:", feature_annotations(myf))
-    print("__annotations__:", MyFeatures.__annotations__)
